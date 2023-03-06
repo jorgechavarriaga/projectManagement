989461afe7384d8ae21bdb5538e280557ff4b85d
Jorge Chavarriaga <jorge.chavarriaga@gmail.com>
2023-03-06 10:28:14 -0500

    Association between User and Projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_03_214542) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_03_215038) do
   create_table "projects", force: :cascade do |t|
     t.string "tittle"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "tasks", force: :cascade do |t|
